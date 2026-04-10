f75b511a705454fb53f06bda195700054a421531
The World <codeberg.limeade108@passmail.net>
2026-04-10 18:55:54 +0200

    Added authentication provider detection

--- src-tauri/src/authentication/login.rs
+++ src-tauri/src/authentication/login.rs
@@ -4,18 +4,33 @@ use serde::Deserialize;
 use crate::AppState;
 
 #[derive(Debug, Deserialize)]
-struct MatrixConfig {
+pub struct WellKnown {
     #[serde(rename = "m.homeserver")]
     pub homeserver: HomeServer,
+
+    #[serde(rename = "org.matrix.msc2965.authentication")]
+    pub authentication: Authentication,
 }
 
 #[derive(Debug, Deserialize)]
-struct HomeServer {
+pub struct HomeServer {
     #[serde(rename = "base_url")]
     pub base_url: String,
 }
 
-pub(crate) async fn get_well_known(_state: &AppState, website: String) -> anyhow::Result<String> {
+#[derive(Debug, Deserialize)]
+pub struct Authentication {
+    #[serde(rename = "issuer")]
+    pub authentication_url: String,
+
+    #[serde(rename = "account")]
+    pub account_url: String,
+}
+
+pub(crate) async fn get_well_known(
+    _state: &AppState,
+    website: String,
+) -> anyhow::Result<WellKnown> {
     let client = reqwest::Client::new();
 
     let res = client
@@ -25,15 +40,13 @@ pub(crate) async fn get_well_known(_state: &AppState, website: String) -> anyhow
         .context("Failed to get /.well-known/matrix/client")?;
 
     if res.status().is_success() {
-        let well_known: MatrixConfig = res
+        let well_known: WellKnown = res
             .json()
             .await
             .context("Failed to parse .well-known response")?;
-        dbg!(&well_known.homeserver.base_url);
-        Ok(well_known.homeserver.base_url)
+        Ok(well_known)
     } else {
-        let content: MatrixConfig = res.json().await?;
-        dbg!(&content);
+        let content: WellKnown = res.json().await?;
         return Err(anyhow!(format!(
             "GET request to /.well-known/matrix/client resulted in an error: {:?}",
             content
