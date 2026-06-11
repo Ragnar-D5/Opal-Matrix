d7db01b6166ca1e56e4e77b5406220c0cd5595fc
Amasica <erik.oberlaender@web.de>
2026-06-11 21:50:27 +0200

    Hardcode Klipy API key

--- src-tauri/src/frontend/klipy.rs
+++ src-tauri/src/frontend/klipy.rs
@@ -6,9 +6,10 @@ use crate::TauriError;
 /// Search klipy using the given string
 #[tauri::command]
 pub async fn search_gifs(search_term: String, page: u32) -> Result<Page<MediaItem>, TauriError> {
-    let klipy = klipy::Klipy::builder(env!("KLIPY_API_KEY"))
-        .http_client(reqwest::Client::new())
-        .build();
+    let klipy =
+        klipy::Klipy::builder("9dEuScySalHgQ4miP7P3q6lYhneUEHJbW2Yft54tfCwJ2uTox3QKGgn0BN2un4mG")
+            .http_client(reqwest::Client::new())
+            .build();
 
     let page = klipy.gifs().search(search_term).page(page).send().await?;
     Ok(page)
