f133f0ceac5112554576fd2c5d8d163c1afd5c4e
Amasica <erik.oberlaender@web.de>
2026-07-12 18:46:04 +0200

    Fixed epstein mode

--- src/components/settings/sections/appearance.rs
+++ src/components/settings/sections/appearance.rs
@@ -11,7 +11,7 @@ pub fn render_appearance_section() -> AnyView {
     view! {
         <Slider field=settings.scaling min=0.0 max=2.0 />
         <Spacer />
-        <Slider field=settings.epstein_mode min=0.0 max=1.0 />
+        <Slider field=settings.epstein_mode min=0.0 max=100.0 />
     }
     .into_any()
 }
