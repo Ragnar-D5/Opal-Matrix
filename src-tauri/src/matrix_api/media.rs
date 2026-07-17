a6c14251ea6d3d4be8e36ffd05e90f7cb4198d0f
Acisama <erik.oberlaender@web.de>
2026-07-17 11:29:49 +0200

    New version

--- src-tauri/src/matrix_api/media.rs
+++ src-tauri/src/matrix_api/media.rs
@@ -1,7 +1,7 @@
 use matrix_sdk::{
     Client,
     media::{MediaFormat, MediaRequestParameters, MediaThumbnailSettings},
-    ruma::{OwnedRoomId, OwnedUserId, api::client::profile::AvatarUrl, events::room::MediaSource},
+    ruma::events::room::MediaSource,
 };
 use tauri::{State, command};
 use tokio::sync::RwLock;
@@ -40,59 +40,4 @@ pub async fn get_thumbnail(
 ) -> Result<tauri::ipc::Response, TauriError> {
     let client = client.read().await;
     fetch_media(&client, source, MediaFormat::Thumbnail(settings)).await
-}
-
-#[command(rename_all = "snake_case")]
-pub async fn get_user_avatar(
-    client: State<'_, RwLock<Client>>,
-    user_id: OwnedUserId,
-) -> Result<tauri::ipc::Response, TauriError> {
-    let client = client.read().await;
-
-    let profile = client.account().fetch_user_profile_of(&user_id).await?;
-
-    let avatar_url = profile
-        .get_static::<AvatarUrl>()?
-        .ok_or("No avatar available")?;
-
-    fetch_media(&client, MediaSource::Plain(avatar_url), MediaFormat::File).await
-}
-
-#[command(rename_all = "snake_case")]
-pub async fn get_room_avatar(
-    client: State<'_, RwLock<Client>>,
-    room_id: OwnedRoomId,
-) -> Result<tauri::ipc::Response, TauriError> {
-    let client = client.read().await;
-
-    let room = client
-        .get_room(&room_id)
-        .ok_or(format!("Room {room_id} not found"))?;
-    let bytes = room
-        .avatar(MediaFormat::File)
-        .await?
-        .ok_or("No avatar available")?;
-
-    Ok(tauri::ipc::Response::new(bytes))
-}
-
-#[command(rename_all = "snake_case")]
-pub async fn get_member_avatar(
-    client: State<'_, RwLock<Client>>,
-    room_id: OwnedRoomId,
-    user_id: OwnedUserId,
-) -> Result<tauri::ipc::Response, TauriError> {
-    let client = client.read().await;
-
-    let room = client.get_room(&room_id).ok_or("No room available")?;
-    let member = room
-        .get_member_no_sync(&user_id)
-        .await?
-        .ok_or(format!("Member {user_id} not found in room {room_id}"))?;
-    let bytes = member
-        .avatar(MediaFormat::File)
-        .await?
-        .ok_or("No avatar available")?;
-
-    Ok(tauri::ipc::Response::new(bytes))
 }
